8de9c887b3d0cbaca1115cc77532a333c404d0b2
jeffersonmacedocarvalho <131269358+jeffersonmacedocarvalho@users.noreply.github.com>
2023-05-11 09:18:08 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,131 @@
+// const nome = String(prompt("informe seu nome:"))
+// const cor = String(prompt(`olá  ${nome} agora informe sua cor favorita:`))
+
+// console.log(`a cor favorita do ${nome} é ${cor}`);
+
+//---------------------------------------------------------------------
+
+// const nome = "                                    jose maria"
+
+// console.log(nome.length);
+// console.log(nome.toUpperCase());
+// //JOSE MARIA
+
+// const nome2 = "                                    JOSE MARIA"
+
+// console.log(nome2.trim());
+// //JOSE MARIA
+// console.log(nome2.trim().toLowerCase());
+// //jose maria
+// console.log(nome2.includes("jose"));
+// //nome existe(true)
+// console.log(nome2.toLowerCase().includes("jose maria"));
+// //este nome existe em minusculo
+// console.log(nome2.trim().toLowerCase().includes("jose maria"));
+// //procurando nome existente, tirando os espaços e colocando em minusculo
+
+// const N = nome.repleceall("jose","carlos")
+
+// console.log("o novo nome;", N.length());
+
+//--------------------------------------------------------------------------------
+
+// const frase = String(prompt("escreva alguma frase:"))
+
+// console.log(frase.toUpperCase());
+
+// const frase2 = frase.toLowercase().replaceAll("O","I");
+
+// console.log(frase2.length);
+
+//-------------------------------------------------------------
+
+// const lc = ["batata","detergente","biscoito"]
+// console.log(lc[1]);
+
+//-------------------------------------------------------------------------------
+
+// const cachorros = ["labrador","caramelo","dalmata","golden","vira-lata"]
+// const i = Number(prompt("informe o numero da posição que deseja:"))
+
+// console.log(cachorros[i -1]);
+
+//----------------------------------------------------------------------------
+
+// const pokemons = ["pikachu","lugia","braviary","salamence","lunala"]
+
+// console.log(pokemons.length);
+// console.log(pokemons.includes("lugia"));
+// pokemons.push("frokie")
+// console.log(pokemons);
+// pokemons.pop();
+// console.log(pokemons);
+// pokemons.splice(2,1)
+// console.log(pokemons);
+
+//--------------------------------------------------------------------------------
+
+// const num = [1, 2, 3, 4, 5, 6]
+
+// console.log("tamanho do array ", num.length);
+// console.log(num);
+// num.push(7)
+// console.log(num);
+// num.splice(3, 2);
+// console.log("novo tamanho do array ", num.length);
+// console.log(num);
+
+//--------------------------------------------------------------------------------
+
+// const nome = prompt("informe seu nome por favor:")
+// const email = prompt(`informe seu email ${nome}:`)
+// console.log(`seu email foi cadastrado com sucesso ${nome} `);
+// console.log(`email cadastrado:${email}`);
+
+//--------------------------------------------------------------------------------
+
+// const C = ["massa","salada","arroz","feijão","peixe"]
+
+// console.log(`essas sao minhas comidas favoritas:${C[0]} \n ${C[1]} \n ${C[2]} \n ${C[3]} \n ${C[4]}`);
+
+// C[1] = prompt("informe uma comida que você gosta")
+// console.log(C);
+
+//-----------------------------------------------------------------------------------------------------------
+
+// let ltd = []
+// const l = prompt("informe a sua primeira tarefa:")
+// ltd.push(l)
+
+// const t = prompt("informe sua segunda tarefa:") 
+// ltd.push(t)
+
+// const d = prompt("informe sua terceira tarefa:")
+// ltd.push(d)
+
+// console.log(ltd);
+
+// const i = prompt("informe a tarefa que ja concluiu:")
+// const Q = prompt("informe quantos itens foram realizados:")
+// ltd.splice(i -1, Q)
+// console.log(`sobram isso ainda para realizar: ${ltd}`);
+
+//-----------------------------------------------------------------
+
+// const frase =  prompt("informe uma frase:")
+// const array = frase.split(" ")
+// console.log(array);
+// console.log(array.length);
+
+// const i = prompt("informe a palavra que deseja procurar:")
+// const index = array.findIndex(checkAge);
+
+// function checkAge (age){
+//     return age === i;
+// }
+// console.log(index);
+
+//-----------------------------------------------------------
+
+
+
